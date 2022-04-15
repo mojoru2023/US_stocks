f2eb387390cba5b2e87a5396f5387eed3f415afc
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2022-04-15 10:12:28 +0900

    Update merage_ontime_industry_netprofit.py

--- ForLynne/5. 超越阿尔法3%的前30只股票计算/merage_ontime_industry_netprofit.py
+++ ForLynne/5. 超越阿尔法3%的前30只股票计算/merage_ontime_industry_netprofit.py
@@ -1,4 +1,5 @@
 
+
 # js   js_infos_finandata
 # coding industry title market
 import csv
@@ -30,46 +31,55 @@ import datetime
 
 
 
-def merge_3table(tablename):
-    engine_Lynne_Mons = create_engine('mysql+pymysql://root:123456@localhost:3306/ForLynne')
+def top2index_top30_and_last30_stock(tablename,dbname):
+    # 1 首先前2位是股指,后面都是个股(计算好的超越贝塔的收益率数据)
+# 2 先保证最近的50个个股不为空的数量大于三分之一
+# 3 以最后一个收益率进行排序。输出代码和收益率
+# 4 返回代码和带代码的收益率
+    engine_Lynne_Mons = create_engine('mysql+pymysql://root:123456@localhost:3306/{0}'.format(dbname))
     sql_Mons = 'select * from {0}  ; '.format(tablename)
     df_sp_plus_nas = pd.read_sql_query(sql_Mons, engine_Lynne_Mons)
     sp500_plus_nas100_forDB = "INX,IXIC,AAPL,A,AAL,AAP,ABBV,ABC,ABMD,ABT,ACN,ADBE,ADI,ADM,ADP,ADS,ADSK,AEE,AEP,AES,AFL,AIG,AIV,AIZ,AJG,AKAM,ALB,ALGN,ALK,_ALL,ALLE,ALXN,AMAT,AMCR,AMD,AME,AMGN,AMP,AMT,AMZN,ANET,ANSS,ANTM,AON,AOS,APA,APD,APH,APTV,ARE,ASML,ATO,ATVI,AVB,AVGO,AVY,AWK,AXP,AZO,BA,BAC,BAX,BBY,BDX,BEN,BF_B,BIDU,BIIB,BK,BKNG,BKR,BLK,BLL,BMRN,BMY,BR,BRK_B,BSX,BWA,BXP,C,CAG,CAH,CARR,CAT,CB,CBOE,CBRE,CCI,CCL,CDNS,CDW,CE,CERN,CF,CFG,CHD,CHKP,CHRW,CHTR,CI,CINF,CL,CLX,CMA,CMCSA,CME,CMG,CMI,CMS,CNC,CNP,COF,COG,COO,COP,COST,COTY,CPB,CPRT,CRM,CSCO,CSGP,CSX,CTAS,CTL,CTSH,CTVA,CTXS,CVS,CVX,CXO,D,DAL,DD,DE,DFS,DG,DGX,DHI,DHR,DIS,DISCA,DISCK,DISH,DLR,DLTR,DOV,DOW,DPZ,DRE,DRI,DTE,DUK,DVA,DVN,DXC,DXCM,EA,EBAY,ECL,ED,EFX,EIX,EL,EMN,EMR,EOG,EQIX,EQR,ES,ESS,ETFC,ETN,ETR,EVRG,EW,EXC,EXPD,EXPE,EXR,F,FANG,FAST,FB,FBHS,FCX,FDX,FE,FFIV,FIS,FISV,FITB,FLIR,FLS,FLT,FMC,FOX,FOXA,FRC,FRT,FTI,FTNT,FTV,GD,GE,GILD,GIS,GL,GLW,GM,GOOG,GOOGL,GPC,GPN,GPS,GRMN,GS,GWW,HAL,HAS,HBAN,HBI,HCA,HD,HES,HFC,HIG,HII,HLT,HOG,HOLX,HON,HPE,HPQ,HRB,HRL,HSIC,HST,HSY,HUM,HWM,IBM,ICE,IDXX,IEX,IFF,ILMN,INCY,INFO,INTC,INTU,IP,IPG,IPGP,IQV,IR,IRM,ISRG,IT,ITW,IVZ,J,JBHT,JCI,JD,JKHY,JNJ,JNPR,JPM,JWN,K,_KEY,_KEYS,KHC,KIM,KLAC,KMB,KMI,KMX,KO,KR,KSS,KSU,L,LB,LBTYA,LBTYK,LDOS,LEG,LEN,LH,LHX,LIN,LKQ,LLY,LMT,LNC,LNT,LOW,LRCX,LULU,LUV,LVS,LW,LYB,LYV,MA,MAA,MAR,MAS,MCD,MCHP,MCK,MCO,MDLZ,MDT,MELI,MET,MGM,MHK,MKC,MKTX,MLM,MMC,MMM,MNST,MO,MOS,MPC,MRK,MRO,MS,MSCI,MSFT,MSI,MTB,MTD,MU,MXIM,MYL,NBL,NCLH,NDAQ,NEE,NEM,NFLX,NI,NKE,NLOK,NLSN,NOC,NOV,NOW,NRG,NSC,NTAP,NTES,NTRS,NUE,NVDA,NVR,NWL,NWS,NWSA,NXPI,O,ODFL,OKE,OMC,ORCL,ORLY,OTIS,OXY,PAYC,PAYX,PBCT,PCAR,PEAK,PEG,PEP,PFE,PFG,PG,PGR,PH,PHM,PKG,PKI,PLD,PM,PNC,PNR,PNW,PPG,PPL,PRGO,PRU,PSA,PSX,PVH,PWR,PXD,PYPL,QCOM,QRVO,RCL,RE,REG,REGN,RF,RHI,RJF,RL,RMD,ROK,ROL,ROP,ROST,RSG,RTX,SBAC,SBUX,SCHW,SEE,SGEN,SHW,SIRI,SIVB,SJM,SLB,SLG,SNA,SNPS,SO,SPG,SPGI,SPLK,SRE,STE,STT,STX,STZ,SWK,SWKS,SYF,SYK,SYY,T,TAP,TCOM,TDG,TEL,TFC,TFX,TGT,TIF,TJX,TMO,TMUS,TPR,TROW,TRV,TSCO,TSLA,TSN,TT,TTWO,TWTR,TXN,TXT,UA,UAA,UAL,UDR,UHS,ULTA,UNH,UNM,UNP,UPS,URI,USB,V,VAR,VFC,VIAC,VLO,VMC,VNO,VRSK,VRSN,VRTX,VTR,VZ,WAB,WAT,WBA,WDAY,WDC,WEC,WELL,WFC,WHR,WLTW,WM,WMB,WMT,WRB,WRK,WST,WU,WY,WYNN,XEL,XLNX,XOM,XRAY,XRX,XYL,YUM,ZBH,ZBRA,ZION,ZTS"
     list_sp500_plus_nas100_forSearch = sp500_plus_nas100_forDB.split(",")
-    data_list =[]
-    top30_code =[]
+    data_list = []
     # count top 30
     for one_stock in list_sp500_plus_nas100_forSearch:
-        if list(df_sp_plus_nas[one_stock])[0] !=0:
-            # 近50条记录里面为空的 小于15个
-            if df_sp_plus_nas[one_stock].tolist()[-50:].count("") < 15:
-
-                value = (float(list(df_sp_plus_nas[one_stock])[-1])-list(df_sp_plus_nas[one_stock])[0])/list(df_sp_plus_nas[one_stock])[0]
-                value_4 = round(value,4)
-                new_dict = {"code":one_stock,"value":value_4}
-                data_list.append(new_dict)
+        # 2 先保证最近的50个个股不为空的数量大于三分之一
+        if df_sp_plus_nas[one_stock].tolist()[-50:].count("") < 15:
+            # 这里只是比较提取,不做计算
+        # 3 以最后一个收益率进行排序。输出代码和收益率
+            last_value = df_sp_plus_nas[-1:][one_stock].tolist()
+            new_dict = {"code": one_stock, "value": last_value}
+            data_list.append(new_dict)
 
 
     # 上面排序取前30个标的
-    sort_data = sorted(data_list,key=operator.itemgetter('value'), reverse=True)
-    for item in sort_data[:30]:
-        top30_code.append(item["code"])
+    sort_data_top30 = sorted(data_list, key=operator.itemgetter('value'), reverse=True)
+    sort_data_last30 = sorted(data_list, key=operator.itemgetter('value'), reverse=False)
 
+    top30_item_dict = sort_data_top30[:30]
+    last30_item_dict = sort_data_last30[:30]
 
+    # top30 last30 code
+    top30_code_list = sort_data_top30[:30].keys().tolist()
+    last30_code_list = sort_data_last30[:30].keys().tolist()
 
-    # outout top30
-    top30_code = ",".join(top30_code)
-    print(top30_code)
-    # sql_Mons_top30 = 'select {1} from {0}  ; '.format(tablename,top30_code)
-    # df_top30 = pd.read_sql_query(sql_Mons_top30, engine_Lynne_Mons)
-    # excelFile = 'ontime-{0}.xlsx'.format(tablename)  # 处理了文件属于当前目录下！
-    # df_top30.to_excel(excelFile)
+    return top30_code_list,last30_code_list,top30_item_dict,last30_item_dict
 
 
 
 
-    for item in sort_data[:30]:
-        print(item["code"],item["value"])
+def output_dt_use_code(code_list,excelfilename,dbname,tablename):
+    fordb_select_code = ",".join(code_list)
+    engine_Mons = create_engine('mysql+pymysql://root:123456@localhost:3306/{0}'.format(dbname))
+    sql_f = 'select {0} from {1}  ; '.format(fordb_select_code,tablename)
+    df_f = pd.read_sql_query(sql_f.format(",".join(get_code)), engine_Mons)
+    df_f.to_excel("{0}-{1}.xlsx".format(excelfilename,tablename))
+
+
+
+def merge_3table(tablename,itemlist):
+    for item in itemlist:
     # get netprofits
         connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='ForLynne',
                                      charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
@@ -104,45 +114,15 @@ def merge_3table(tablename):
 
 
 
-def merge_industryPlusNetProfits():
 
-    sp500_plus_nas100_forDB = "INX,IXIC,AAPL,A,AAL,AAP,ABBV,ABC,ABMD,ABT,ACN,ADBE,ADI,ADM,ADP,ADS,ADSK,AEE,AEP,AES,AFL,AIG,AIV,AIZ,AJG,AKAM,ALB,ALGN,ALK,_ALL,ALLE,ALXN,AMAT,AMCR,AMD,AME,AMGN,AMP,AMT,AMZN,ANET,ANSS,ANTM,AON,AOS,APA,APD,APH,APTV,ARE,ASML,ATO,ATVI,AVB,AVGO,AVY,AWK,AXP,AZO,BA,BAC,BAX,BBY,BDX,BEN,BF_B,BIDU,BIIB,BK,BKNG,BKR,BLK,BLL,BMRN,BMY,BR,BRK_B,BSX,BWA,BXP,C,CAG,CAH,CARR,CAT,CB,CBOE,CBRE,CCI,CCL,CDNS,CDW,CE,CERN,CF,CFG,CHD,CHKP,CHRW,CHTR,CI,CINF,CL,CLX,CMA,CMCSA,CME,CMG,CMI,CMS,CNC,CNP,COF,COG,COO,COP,COST,COTY,CPB,CPRT,CRM,CSCO,CSGP,CSX,CTAS,CTL,CTSH,CTVA,CTXS,CVS,CVX,CXO,D,DAL,DD,DE,DFS,DG,DGX,DHI,DHR,DIS,DISCA,DISCK,DISH,DLR,DLTR,DOV,DOW,DPZ,DRE,DRI,DTE,DUK,DVA,DVN,DXC,DXCM,EA,EBAY,ECL,ED,EFX,EIX,EL,EMN,EMR,EOG,EQIX,EQR,ES,ESS,ETFC,ETN,ETR,EVRG,EW,EXC,EXPD,EXPE,EXR,F,FANG,FAST,FB,FBHS,FCX,FDX,FE,FFIV,FIS,FISV,FITB,FLIR,FLS,FLT,FMC,FOX,FOXA,FRC,FRT,FTI,FTNT,FTV,GD,GE,GILD,GIS,GL,GLW,GM,GOOG,GOOGL,GPC,GPN,GPS,GRMN,GS,GWW,HAL,HAS,HBAN,HBI,HCA,HD,HES,HFC,HIG,HII,HLT,HOG,HOLX,HON,HPE,HPQ,HRB,HRL,HSIC,HST,HSY,HUM,HWM,IBM,ICE,IDXX,IEX,IFF,ILMN,INCY,INFO,INTC,INTU,IP,IPG,IPGP,IQV,IR,IRM,ISRG,IT,ITW,IVZ,J,JBHT,JCI,JD,JKHY,JNJ,JNPR,JPM,JWN,K,_KEY,_KEYS,KHC,KIM,KLAC,KMB,KMI,KMX,KO,KR,KSS,KSU,L,LB,LBTYA,LBTYK,LDOS,LEG,LEN,LH,LHX,LIN,LKQ,LLY,LMT,LNC,LNT,LOW,LRCX,LULU,LUV,LVS,LW,LYB,LYV,MA,MAA,MAR,MAS,MCD,MCHP,MCK,MCO,MDLZ,MDT,MELI,MET,MGM,MHK,MKC,MKTX,MLM,MMC,MMM,MNST,MO,MOS,MPC,MRK,MRO,MS,MSCI,MSFT,MSI,MTB,MTD,MU,MXIM,MYL,NBL,NCLH,NDAQ,NEE,NEM,NFLX,NI,NKE,NLOK,NLSN,NOC,NOV,NOW,NRG,NSC,NTAP,NTES,NTRS,NUE,NVDA,NVR,NWL,NWS,NWSA,NXPI,O,ODFL,OKE,OMC,ORCL,ORLY,OTIS,OXY,PAYC,PAYX,PBCT,PCAR,PEAK,PEG,PEP,PFE,PFG,PG,PGR,PH,PHM,PKG,PKI,PLD,PM,PNC,PNR,PNW,PPG,PPL,PRGO,PRU,PSA,PSX,PVH,PWR,PXD,PYPL,QCOM,QRVO,RCL,RE,REG,REGN,RF,RHI,RJF,RL,RMD,ROK,ROL,ROP,ROST,RSG,RTX,SBAC,SBUX,SCHW,SEE,SGEN,SHW,SIRI,SIVB,SJM,SLB,SLG,SNA,SNPS,SO,SPG,SPGI,SPLK,SRE,STE,STT,STX,STZ,SWK,SWKS,SYF,SYK,SYY,T,TAP,TCOM,TDG,TEL,TFC,TFX,TGT,TIF,TJX,TMO,TMUS,TPR,TROW,TRV,TSCO,TSLA,TSN,TT,TTWO,TWTR,TXN,TXT,UA,UAA,UAL,UDR,UHS,ULTA,UNH,UNM,UNP,UPS,URI,USB,V,VAR,VFC,VIAC,VLO,VMC,VNO,VRSK,VRSN,VRTX,VTR,VZ,WAB,WAT,WBA,WDAY,WDC,WEC,WELL,WFC,WHR,WLTW,WM,WMB,WMT,WRB,WRK,WST,WU,WY,WYNN,XEL,XLNX,XOM,XRAY,XRX,XYL,YUM,ZBH,ZBRA,ZION,ZTS"
-    list_sp500_plus_nas100_forSearch = sp500_plus_nas100_forDB.split(",")
-    # count top 30
-    for one_stock in list_sp500_plus_nas100_forSearch:
-    # get netprofits
-        connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='ForLynne',
-                                     charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-        cur = connection.cursor()
-        # sql 语句
-        sql_netprofits = "select name,current_dt,current_dt_minus1,current_dt_minus2,current_dt_minus3,current_dt_minus4,sina_stock_url from sp_nas_netprofits_fromsina  where code = '{0}'   ".format(
-            one_stock)
-        cur.execute(sql_netprofits)
-        # #获取所有记录列表
-        data_sql_netprofits = cur.fetchone()
-        current_dt = data_sql_netprofits["current_dt"]
-        current_dt_minus1 = data_sql_netprofits["current_dt_minus1"]
-        current_dt_minus2 = data_sql_netprofits["current_dt_minus2"]
-        current_dt_minus3 = data_sql_netprofits["current_dt_minus3"]
-        current_dt_minus4 = data_sql_netprofits["current_dt_minus4"]
-        sina_stock_url = data_sql_netprofits["sina_stock_url"]
 
-    # get industry data
-        sql_industry_info = "select title_zh_cn,industry_infos,sector_infos,infos_zh_cn from SpPlusNas_industry_infos where code = '{0}'   ".format(
-            one_stock)
-        cur.execute(sql_industry_info)
-        # #获取所有记录列表
-        sql_industry_info = cur.fetchone()
-        print(sql_industry_info)
-        title_zh_cn = sql_industry_info["title_zh_cn"]
-        industry_infos = sql_industry_info["industry_infos"]
-        sector_infos = sql_industry_info["sector_infos"]
-        infos_zh_cn = sql_industry_info["infos_zh_cn"]
-        three_table_info = [one_stock,title_zh_cn,industry_infos,sector_infos,sina_stock_url,current_dt,current_dt_minus1,current_dt_minus2,current_dt_minus3,current_dt_minus4,infos_zh_cn]
-        time_ = datetime.datetime.now().strftime("%Y-%m-%d")
-        writeinto_detail("{0}-industryInfo-NetProfits.csv".format(time_), three_table_info)
-        cur.close()
-
+def load_ontimedt_and_detailsDt(dbname,tablename):
+    top30_code_list, last30_code_list, top30_item_dict, last30_item_dict = top2index_top30_and_last30_stock(tablename, dbname)
+    output_dt_use_code(top30_code_list, "top30", dbname, tablename)
+    output_dt_use_code(last30_code_list, "last30",dbname,tablename )
+    merge_3table(top30_item_dict,tablename)
+    merge_3table(last30_item_dict,tablename)
+    
 
 
 def writeinto_detail(filename,data):
@@ -150,9 +130,8 @@ def writeinto_detail(filename,data):
         csv_out = csv.writer(f,delimiter=",")
         csv_out.writerow(data)
 
-
 if __name__== "__main__":
-    merge_3table("following_sp500_plus_nas100_20220412")
-
-
-# select INX,IXIC from following_sp500_plus_nas100_20220412;
+    # 这里导出top30,last30数据
+    dbname = "ForLynne"
+    tablename = "following_sp500_plus_nas100_20220412"
+    load_ontimedt_and_detailsDt(dbname,tablename)
